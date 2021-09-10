b9c1437ca112a988af1cb0d932cdc2dc203bec75
utechnique <universal_tools@protonmail.com>
2021-09-10 19:05:07 +0300

    Temporary fix for Linux vkCreateFence() error on resizing.

--- source/ve/src/systems/render/engine/ve_render_toolset.cpp
+++ source/ve/src/systems/render/engine/ve_render_toolset.cpp
@@ -24,7 +24,11 @@ Shader::Macros GenQuadShaderConversionMacros(bool rgb_to_srgb)
 //----------------------------------------------------------------------------//
 // Constructor.
 Toolset::Toolset(Device& dvc_ref) : device(dvc_ref)
+#if UT_LINUX
+                                  , pool(ut::Min(ut::GetNumberOfProcessors(), 4))
+#else
                                   , pool(ut::GetNumberOfProcessors())
+#endif
                                   , scheduler(pool.CreateScheduler())
                                   , config(LoadCfg())
                                   , rc_mgr(dvc_ref, config)
