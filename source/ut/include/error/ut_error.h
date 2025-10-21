4972a98fda19e756851a7244c93f51ef153169a8
utechnique <universal_tools@protonmail.com>
2025-10-21 22:08:20 +0100

    Better ut::Error description.

--- source/ut/include/error/ut_error.h
+++ source/ut/include/error/ut_error.h
@@ -32,13 +32,27 @@ public:
 	//    @param backtrace - indicates if a callstack must be built.
 	Error(error::Code error_code, String error_desc, bool backtrace = false);
 
+	// Constructor
+	//    @param error_code - error code, see ut::error::Code enumeration
+	//    @param error_desc - null-terminated C-string with custom
+	//                        (user-defined) error description
+	//    @param backtrace - indicates if a callstack must be built.
+	Error(error::Code error_code,
+	      const char* error_desc,
+	      bool backtrace = false) : Error(error_code,
+	                                      String(error_desc),
+	                                      backtrace)
+	{}
+
 	// Returns error code
 	//    @return - error code, see ut::error::Code enumeration
 	error::Code GetCode() const;
 
 	// Returns full description of the error
+	// 	  @param short_form - indicates whether to form a description
+	//                        in a short single line form without a backtrace.
 	//    @return - string with error description and call stack
-	const String GetDesc() const;
+	const String GetDesc(bool short_form = false) const;
 
 private:
 	// Returns a string with a callstack (every call has a separate line)
